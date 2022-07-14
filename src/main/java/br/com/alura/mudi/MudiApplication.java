4ed9c4ad729c40792125d917402d56715a4c0362
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-14 17:31:53 -0300

    Criando Modelo

--- src/main/java/br/com/alura/mudi/MudiApplication.java
+++ src/main/java/br/com/alura/mudi/MudiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MudiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MudiApplication.class, args);
 	}
 
