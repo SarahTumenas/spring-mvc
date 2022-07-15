bf95da1b667325c76fdd855e2218dad2dfad2e31
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-15 16:18:34 -0300

    Usando JPA puro

--- src/main/java/br/com/alura/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mudi/controller/HomeController.java
@@ -1,30 +1,27 @@
 package br.com.alura.mudi.controller;
 
+
 import br.com.alura.mudi.model.Pedido;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
+
 
 @Controller
 public class HomeController {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
         @GetMapping("/home")
         public String home(Model model) {
-            Pedido pedido = new Pedido();
-            pedido.setNomeProduto("Xiaomi Redmi Note 11");
-            pedido.setValorNegociado(new BigDecimal("1379.99"));
-            pedido.setDataDaEntrega(LocalDate.now());
-            pedido.setUrlProduto("https://www.amazon.com.br/Xiaomi-Redmi-Note-11-Graphite/dp/B09QSB4N2C/ref=sr_1_7?keywords=xiaomi&qid=1657832416&sr=8-7&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
-            pedido.setUrlImagem("https://m.media-amazon.com/images/I/51e3KdrHuCL._AC_SL1080_.jpg");
-            pedido.setDescricao("Xiaomi Redmi Note 11 - Graphite Gray 6GB Ram 128GB Rom");
-
-
-            List<Pedido> pedidos = Arrays.asList(pedido);
+            Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+            List<Pedido> pedidos = query.getResultList();
 
             model.addAttribute("pedidos", pedidos);
 
--- src/main/java/br/com/alura/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mudi/model/Pedido.java
@@ -1,9 +1,17 @@
 package br.com.alura.mudi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nomeProduto;
     private BigDecimal valorNegociado;
