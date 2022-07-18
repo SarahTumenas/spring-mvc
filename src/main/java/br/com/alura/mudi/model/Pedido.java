b2f2b9540c8da9278d7a7bf373155d59574712be
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-18 17:13:33 -0300

    Usando Vue.js

--- src/main/java/br/com/alura/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mudi/model/Pedido.java
@@ -1,13 +1,14 @@
 package br.com.alura.mudi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
 public class Pedido {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nomeProduto;
@@ -17,73 +18,65 @@ public class Pedido {
     private String urlImagem;
     private String descricao;
 
-    @ManyToOne (fetch = FetchType.LAZY)
-    private User user;
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
     @Enumerated(EnumType.STRING)
     private StatusPedido status;
 
-    public void setStatus(StatusPedido status) {
-        this.status = status;
-    }
-
-    public StatusPedido getStatus() {
-        return status;
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
 
     public String getNomeProduto() {
         return nomeProduto;
     }
-
     public void setNomeProduto(String nomeProduto) {
         this.nomeProduto = nomeProduto;
     }
-
     public BigDecimal getValorNegociado() {
         return valorNegociado;
     }
-
     public void setValorNegociado(BigDecimal valorNegociado) {
         this.valorNegociado = valorNegociado;
     }
-
     public LocalDate getDataDaEntrega() {
         return dataDaEntrega;
     }
-
     public void setDataDaEntrega(LocalDate dataDaEntrega) {
         this.dataDaEntrega = dataDaEntrega;
     }
-
     public String getUrlProduto() {
         return urlProduto;
     }
-
     public void setUrlProduto(String urlProduto) {
         this.urlProduto = urlProduto;
     }
-
     public String getUrlImagem() {
         return urlImagem;
     }
-
     public void setUrlImagem(String urlImagem) {
         this.urlImagem = urlImagem;
     }
-
     public String getDescricao() {
         return descricao;
     }
-
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public StatusPedido getStatus() {
+        return status;
+    }
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
     }
 }
--- src/main/java/br/com/alura/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mudi/repository/PedidoRepository.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-    @Cacheable("pedidos")
+    @Cacheable("books")
     List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 
     @Query("select p from Pedido p join p.user u where u.username = :username")
