56b0622dccb6d360b4c5f8e97d6d3ac98da32c65
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-18 16:05:52 -0300

    Criando RestController

---
+++ src/main/java/br/com/alura/mudi/api/PedidosRest.java
@@ -0,0 +1,28 @@
+package br.com.alura.mudi.api;
+
+import br.com.alura.mudi.model.Pedido;
+import br.com.alura.mudi.model.StatusPedido;
+import br.com.alura.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pedidos")
+public class PedidosRest {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @GetMapping("aguardando")
+    public List<Pedido> getPedidosAguardandoOfertas() {
+        Sort sort = Sort.by("id").descending();
+        PageRequest paginacao = PageRequest.of(0, 10, sort);
+        return pedidoRepository.findByStatus(StatusPedido.AGUARDANDO, paginacao);
+    }
+}
