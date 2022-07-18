9b6743d33e8345c50a0c2031003a073b5d66bd69
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-18 15:35:42 -0300

    Habilitando o cache

--- src/main/java/br/com/alura/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mudi/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package br.com.alura.mudi.repository;
 
 import br.com.alura.mudi.model.Pedido;
 import br.com.alura.mudi.model.StatusPedido;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,6 +15,8 @@ import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    @Cacheable("pedidos")
     List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 
     @Query("select p from Pedido p join p.user u where u.username = :username")
